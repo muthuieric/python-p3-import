6fae617937c357f6e69821a2037ddf6aab5bc454
Eric <ericmuthuipatch22@gmail.com>
2023-08-23 12:38:55 +0300

    completed assignment

---
+++ lib/absolute_package/import_test.py
@@ -0,0 +1,11 @@
+from package1 import module1
+
+module1.function1()
+
+from package1.module1 import function1
+
+function1()
+
+from package2.subpackage1.module6 import function1
+
+function1()
---
+++ lib/relative_package/__init__.py
@@ -0,0 +1,4 @@
+from . import module1
+from . import module2
+
+from .module2 import function1
